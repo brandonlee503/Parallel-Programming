0d4927ed3f0b1e551ab208c86deacb47ed972cf2
Brandon Lee <brandonlee503@gmail.com>
2017-05-23 19:14:32 -0700

    Cleanup and and file io

--- project5/arraymult.cpp
+++ project5/arraymult.cpp
@@ -1,6 +1,7 @@
 #include <omp.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <fstream>
 #include "simd.p5.h"
 
 // Globals
@@ -10,8 +11,10 @@ float ARRAY_C[ARRAY_SIZE];
 
 // Function Prototypes
 float Ranf( float low, float high );
-void SIMD_SSE_MUL();
-void SIMD_SSE_SUM();
+double SIMD_SSE_MUL();
+double SIMD_SSE_SUM();
+double MUL();
+double SUM();
 
 /**
  * Get a random float between the bounds
@@ -27,34 +30,70 @@ float Ranf( float low, float high )
 
 int main( int argc, char *argv[] )
 {
+	// Initialize with random values
 	for (int i = 0; i < ARRAY_SIZE; i++) {
 		ARRAY_A[i] = Ranf(-1.f, 1.f);
 		ARRAY_B[i] = Ranf(-1.f, 1.f);
 	}
 
-	SIMD_SSE_MUL();
-	// printf("\n");
-	SIMD_SSE_SUM();
+	// Write to file
+	std::ofstream outputFile;
+	outputFile.open("outputFile.csv", std::ios_base::app);
+
+	// Perform multiplications
+	double sseMul = SIMD_SSE_MUL();
+	double sseSum = SIMD_SSE_SUM();
+	double mul = MUL();
+	double sum = SUM();
+	printf("\n");
+	outputFile << ARRAY_SIZE << "," << sseMul/mul << "," << sseSum/sum << std::endl;
 }
 
 /**
  * Utilizes SIMD SSE to multiply two arrays
  */
-void SIMD_SSE_MUL() {
+double SIMD_SSE_MUL() {
 	double startTime = omp_get_wtime();
 	SimdMul(ARRAY_A, ARRAY_B, ARRAY_C, ARRAY_SIZE);
 	double endTime = omp_get_wtime();
 	fprintf(stderr, "SIMD_SSE_MUL MegaMults/sec = %10.2lf\n", (double)ARRAY_SIZE/(endTime-startTime)/1000000.);
-	// printf("\n");
+	return (double)ARRAY_SIZE/(endTime-startTime)/1000000.;
 }
 
 /**
  * Utilizes SIMD SSE to multiply and reduce two arrays
  */
-void SIMD_SSE_SUM() {
+double SIMD_SSE_SUM() {
 	double startTime = omp_get_wtime();
 	SimdMulSum(ARRAY_A, ARRAY_B, ARRAY_SIZE);
 	double endTime = omp_get_wtime();
 	fprintf(stderr, "SIMD_SSE_SUM MegaMults/sec = %10.2lf\n", (double)ARRAY_SIZE/(endTime-startTime)/1000000.);
-	// printf("\n");
+	return (double)ARRAY_SIZE/(endTime-startTime)/1000000.;
+}
+
+/**
+ * Utilizes OMP to multiply two arrays
+ */
+double MUL() {
+	double startTime = omp_get_wtime();
+	#pragma omp simd
+	for (int i = 0; i < ARRAY_SIZE; i++) {
+		ARRAY_C[i] = ARRAY_A[i] * ARRAY_B[i];
+	}
+	double endTime = omp_get_wtime();
+	fprintf(stderr, "MUL MegaMults/sec = %10.2lf\n", (double)ARRAY_SIZE/(endTime-startTime)/1000000.);
+	return (double)ARRAY_SIZE/(endTime-startTime)/1000000.;
+}
+
+/**
+ * Multiply and reduce two arrays
+ */
+double SUM() {
+	double startTime = omp_get_wtime();
+	for (int i = 0; i < ARRAY_SIZE; i++) {
+		ARRAY_C[i] = ARRAY_A[i] * ARRAY_B[i] + ARRAY_B[i];
+	}
+	double endTime = omp_get_wtime();
+	fprintf(stderr, "SUM MegaMults/sec = %10.2lf\n", (double)ARRAY_SIZE/(endTime-startTime)/1000000.);
+	return (double)ARRAY_SIZE/(endTime-startTime)/1000000.;
 }
--- project5/getData.sh
+++ project5/getData.sh
@@ -2,19 +2,9 @@
 
 g++ -c simd.p5.cpp -o simd.p5.o
 
-for array_size in 1000 10000 100000 1000000
+MAX_ARR_SIZE=1024*1024*32
+for ((i = 1024; i <= MAX_ARR_SIZE; i *= 2))
 do
-	g++ -DARRAY_SIZE=$array_size -o arraymult arraymult.cpp simd.p5.o -lm -fopenmp
+	g++ -DARRAY_SIZE=$i -o arraymult arraymult.cpp simd.p5.o -lm -fopenmp
 	./arraymult
 done
-
-# Threads
-# for thread in 1 2 4
-# do
-# 	# Performance (Scheduling Type, Chunk Size)
-# 	for numpad in 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15
-# 	do
-# 		g++ -DNUMT=$thread -DNUMPAD=$numpad main.cpp -o main -lm -fopenmp
-# 		./main
-# 	done
-# done
